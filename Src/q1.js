9f406f4620b7e91a87108ca44203e9eb58cb714c
Mateus <mateusdesouzaroseno@gmail.com>
2025-05-19 12:31:36 -0300

    Update q1.js

--- Src/q1.js
+++ Src/q1.js
@@ -1,1 +1,9 @@
+// Questão 1: Verificar se uma variável é do tipo String
+// Conceito: Utilizamos o operador typeof para verificar o tipo de uma variável.
+// Exemplo:
+function isString(str) {
+ return typeof str === 'string';
+}
 
+console.log(isString('w3resource')); // true
+console.log(isString([1, 2, 4, 0])); // false
